18955d629e89ac57d41751b498ef4d3211828d1e
Zakaria Belghit <zakaria.belghit16@gmail.com>
2021-04-07 10:11:43 +0200

    correction_detail_nodeServer

--- accueil.js
+++ accueil.js
@@ -1,49 +1,16 @@
-//Creation d'un tableau mockDataDetail en prenant les mêmes objets que l'API
-const mockDataCamera = [
-    {
-        id : "_id_1",
-        name : "Zurss 50S",
-        imageUrl : "https://images-na.ssl-images-amazon.com/images/I/718sJMJwrhL._AC_SL1500_.jpg",
-        description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        price : "49900"
-    }, 
-    {
-        id : "_id_2",
-        name : "Hirsch 400DTS",
-        imageUrl : "https://images-na.ssl-images-amazon.com/images/I/71f0odyTK9L._AC_SL1300_.jpg",
-        description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        price : "309900"
-    }, 
-    {
-        id : "_id_3",
-        name : "Franck JS 105",
-        imageUrl : "https://images-na.ssl-images-amazon.com/images/I/41E%2BHkyJTfL._AC_.jpg",
-        description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        price : "209900"
-    },
-    {
-        id : "_id_4",
-        name : "Kuros TTS",
-        imageUrl : "https://images-na.ssl-images-amazon.com/images/I/71-TflfsYEL._AC_SL1500_.jpg",
-        description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        price : "159900"
-    }, 
-    {
-        id : "_id_5",
-        name : "Katatone",
-        imageUrl : "https://images-na.ssl-images-amazon.com/images/I/619Qes9U8aL._AC_SL1000_.jpg",
-        description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        price : "59900"
-    } 
-]
-console.log(mockDataCamera)
 
+/*
 const loadMockData = () => {
     return mockDataCamera
 }
+*/
+const loadMockData = async () => {
+    return await (await fetch("http://localhost:3000/api/cameras")).json()
+}
+
 //Affichage dans le HTML de la fonction retournant les objets
-const displayListCameras = () => {
-    const listCameras = loadMockData()
+const displayListCameras = async () => {
+    const listCameras = await loadMockData()
     const listContainer = document.getElementById('container')
 
     for (let i in listCameras) {
@@ -70,7 +37,7 @@ const displayListCameras = () => {
         const camerasPrice = document.createElement("li")
         camerasListDescription.appendChild(camerasPrice)
 
-        camerasContainerElement.setAttribute('href', `details.html?id=${listCameras[i].id}`)
+        camerasContainerElement.setAttribute('href', `details.html?id=${listCameras[i]._id}`)
         camerasImage.setAttribute("src", listCameras[i].imageUrl)
 
         camerasName.innerHTML = listCameras[i].name 
--- details.js
+++ details.js
@@ -1,134 +1,69 @@
-//Creation d'un tableau mockDataDetail en prenant les mêmes objets que l'API
-
-const mockDataDetail = [{
-    id : "_id_1",
-    name : "Zurss 50S",
-    imageUrl : "https://images-na.ssl-images-amazon.com/images/I/718sJMJwrhL._AC_SL1500_.jpg",
-    description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    price : "49900",
-    options : [
-        "bleu",
-        "rouge",
-        "jaune",
-        "violet",
-        "noir"  
-    ]},
-{
-    id : "_id_2",
-    name : "Hirsch 400DTS",
-    imageUrl : "https://images-na.ssl-images-amazon.com/images/I/71f0odyTK9L._AC_SL1300_.jpg",
-    description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    price : "309900",
-    options : [
-        "bleu",
-        "rouge" 
-    ]},
-{
-    id : "_id_3",
-    name : "Franck JS 105",
-    imageUrl : "https://images-na.ssl-images-amazon.com/images/I/41E%2BHkyJTfL._AC_.jpg",
-    description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    price : "209900",
-    options : [
-        "bleu",
-        "rouge",
-        "jaune",
-        "violet",
-        "marron"  
-    ]},
-{
-    id : "_id_4",
-    name : "Kuros TTS",
-    imageUrl : "https://images-na.ssl-images-amazon.com/images/I/71-TflfsYEL._AC_SL1500_.jpg",
-    description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    price : "159900",
-    options : [
-        "bleu",
-        "rouge",
-        "jaune",
-        "violet" 
-    ]},
-{
-    id : "_id_5",
-    name : "Katatone",
-    imageUrl : "https://images-na.ssl-images-amazon.com/images/I/619Qes9U8aL._AC_SL1000_.jpg",
-    description : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    price : "59900",
-    options : [
-        "bleu",
-        "rouge",
-        "violet" 
-    ]}]
-console.log(mockDataDetail)
- 
 
 /* Récupération de la chaine de requête dans l'URL
  * Objectif : cliquer sur un produit du site et être directement renvoyé  à la page détail
 */
 
-const queryString_url_id = window.location.search;
-console.log(queryString_url_id);
-
-
 // Récupération de l'iD, sans le "?"//
+const queryString_url_id = window.location.search;
 
- const urlSearchParams = new URLSearchParams(queryString_url_id)
- console.log(urlSearchParams)
+const urlSearchParams = new URLSearchParams(queryString_url_id)
 
- const id = urlSearchParams.get("id")
- console.log(id)
+const id = urlSearchParams.get("id")
 
- // Récupération grâce à l'id du produit dans détail.html //
-const idChosenProduct = mockDataDetail.find(element => element.id === id)
-console.log(idChosenProduct)
-
-
-const containerListDetail = document.getElementById('detail')
-console.log(containerListDetail )
-
-const detailMainContainer = document.createElement("div")
-containerListDetail.appendChild(detailMainContainer)
-
-const detailName = document.createElement("h2")
-detailMainContainer.appendChild(detailName)
-
-const detailImageUrl = document.createElement("img")
-detailMainContainer.appendChild(detailImageUrl)
-
-detailImageUrl.setAttribute("src", idChosenProduct.imageUrl)
-
-const detailDescription = document.createElement("p")
-detailMainContainer.appendChild(detailDescription)
-
-const detailPrice = document.createElement("p")
-detailMainContainer.appendChild(detailPrice)
-
-
-detailName.innerHTML = " Nom : " + idChosenProduct.name
-detailImageUrl.innerHTML = idChosenProduct.imageUrl
-detailDescription.innerHTML = " Description : " + idChosenProduct.description
-detailPrice.innerHTML = " Prix : " + idChosenProduct.price/100 + " € "
-
-
-//récupération id formulaire pour choix option
-
-const selectionOption = idChosenProduct.options
-console.log(selectionOption)
-
-for (let i in selectionOption){
-    const select = document.getElementById("select")
-    const detailObject = document.createElement("option")
-    select.appendChild(detailObject)
-
-    detailObject.setAttribute("value", `${selectionOption[i]}`)
-    detailObject.innerHTML = idChosenProduct.options[i]
+const loadData = async () => {
+    return await (await fetch("http://localhost:3000/api/cameras/" + id)).json()
 }
 
-//vérification des values
-let checking = document.querySelector("select");
-checking.addEventListener("change", function(){
-    console.log("value => "+this.value)
-})
+//récupération id formulaire pour choix option
+const displayDetail = async () => {
 
+    const idChosenProduct = await loadData()
 
-//récupération id formulaire pour choix option en prenant en compt data Javascript
+    const containerListDetail = document.getElementById('detail')
+    console.log(containerListDetail )
+    
+    const detailMainContainer = document.createElement("div")
+    containerListDetail.appendChild(detailMainContainer)
+    
+    const detailName = document.createElement("h2")
+    detailMainContainer.appendChild(detailName)
+    
+    const detailImageUrl = document.createElement("img")
+    detailMainContainer.appendChild(detailImageUrl)
+    
+    detailImageUrl.setAttribute("src", idChosenProduct.imageUrl)
+    
+    const detailDescription = document.createElement("p")
+    detailMainContainer.appendChild(detailDescription)
+    
+    const detailPrice = document.createElement("p")
+    detailMainContainer.appendChild(detailPrice)
+    
+    
+    detailName.innerHTML = " Nom : " + idChosenProduct.name
+    detailImageUrl.innerHTML = idChosenProduct.imageUrl
+    detailDescription.innerHTML = " Description : " + idChosenProduct.description
+    detailPrice.innerHTML = " Prix : " + idChosenProduct.price/100 + " € "
+    
+    
+    //récupération id formulaire pour choix option
+    
+    const selectionOption = idChosenProduct.lenses
+    console.log(selectionOption)
+    
+    for (let i in selectionOption){
+        const select = document.getElementById("select")
+        const detailObject = document.createElement("option")
+        select.appendChild(detailObject)
+    
+        detailObject.setAttribute("value", `${selectionOption[i]}`)
+        detailObject.innerHTML = idChosenProduct.lenses[i]
+    }
+    
+    //vérification des values
+    let checking = document.querySelector("select");
+        checking.addEventListener("change", function(){
+        console.log("value => "+this.value)
+    })
+}
+displayDetail()
