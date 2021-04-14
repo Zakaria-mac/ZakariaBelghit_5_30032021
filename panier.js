0b26aceac21b96f4f8fddc153e543d9aa85bb803
Zakaria Belghit <zakaria.belghit16@gmail.com>
2021-04-14 12:13:47 +0200

    shopping_card_presentation_ok

--- panier.js
+++ panier.js
@@ -1,8 +1,10 @@
-/* Récupérer le localStorage sur panier.js */
+// Récupérer le localStorage sur panier.js 
 const shoppingCart = async () => {
     return JSON.parse(localStorage.getItem('product'))
 }
-console.log(shoppingCart)
+
+
+// Affichage du panier
 
 const shoppingCartDisplay = async () => {
     const storage = await shoppingCart()
@@ -10,33 +12,55 @@ const shoppingCartDisplay = async () => {
 
     for (let i in storage){
 
-        const containerOfProduct = document.createElement("div")
-        primaryContainer.appendChild(containerOfProduct)
+        const firstContainerShoppingCard = document.createElement("div")
+        primaryContainer.appendChild(firstContainerShoppingCard)
+        firstContainerShoppingCard.setAttribute("class", "card d-flex flex-row")
+        
+        const imageOfProduct = document.createElement("img")
+        firstContainerShoppingCard.appendChild(imageOfProduct)
 
-        const nameOfProduct = document.createElement("h3")
+        const containerOfProduct = document.createElement("div")
+        firstContainerShoppingCard.appendChild(containerOfProduct)
+
+        const nameOfProduct = document.createElement("h2")
         containerOfProduct.appendChild(nameOfProduct)
 
-        const imageofProduct = document.createElement("img")
-        containerOfProduct.appendChild(imageofProduct)
-        imageofProduct.setAttribute("src", storage[i].image)
+        const optionOfProduct = document.createElement("p")
+        containerOfProduct.appendChild(optionOfProduct)
 
+        const priceOfProduct = document.createElement("p")
+        containerOfProduct.appendChild(priceOfProduct)
+
+         // Donne le rôle et la classe de chaque élément créé
+
+        imageOfProduct.setAttribute("src", storage[i].image)
+        imageOfProduct.setAttribute("class", "card-img shoppingCartImg")
+        containerOfProduct.setAttribute("class", "card-body")
+        nameOfProduct.setAttribute("class", "card-title")
+        optionOfProduct.setAttribute("class", "card-text")
+        priceOfProduct.setAttribute("class", "card-text d-flex justify-content-end")
+
+        
+        imageOfProduct.innerHTML = storage[i].image
         nameOfProduct.innerHTML = storage[i].name
-        imageofProduct.innerHTML = storage[i].image
+        optionOfProduct.innerHTML = " Option : " +storage[i].lenses
+        priceOfProduct.innerHTML = storage[i].price
+
+        /* création du bouton pour supprimer un produit*/
+        const deleteProduct = document.getElementsByClassName("btn-danger")
+
+        for (let i = 0; i < deleteProduct.length; i++) {
+        let button = deleteProduct[i]
+        button.addEventListener('click', function(event){
+        let buttonDeletedClicked = event.target 
+        buttonDeletedClicked.firstContainerShoppingCard.remove()
+        })
+        }
+
     }
+       
 }
 shoppingCartDisplay()
 
-
-/* création du bouton pour supprimer un produit*/
-
-const deleteProduct = document.getElementsByClassName("btn-danger")
-
-for (let i = 0; i < deleteProduct.length; i++) {
-    let button = deleteProduct[i]
-    button.addEventListener('click', function(event){
-       let buttonDeletedClicked = event.target 
-       buttonDeletedClicked.parentElement.parentElement.parentElement.remove()
-    })
-}
 
 /* addition du bouton total des prix*/
